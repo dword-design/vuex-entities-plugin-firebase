fce8722e07b58c0fcac56bae280a56c39b7667ec
foo <foo@bar.de>
2020-07-25 19:19:39 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,42 @@
+<!-- TITLE/ -->
+# @dword-design/vuex-entities-plugin-firebase
+<!-- /TITLE -->
+
+<!-- BADGES/ -->
+[![NPM version](https://img.shields.io/npm/v/@dword-design/vuex-entities-plugin-firebase.svg)](https://npmjs.org/package/@dword-design/vuex-entities-plugin-firebase)
+![Linux macOS Windows compatible](https://img.shields.io/badge/os-linux%20%7C%C2%A0macos%20%7C%C2%A0windows-blue)
+[![Build status](https://img.shields.io/github/workflow/status/dword-design/vuex-entities-plugin-firebase/build)](https://github.com/dword-design/vuex-entities-plugin-firebase/actions)
+[![Coverage status](https://img.shields.io/coveralls/dword-design/vuex-entities-plugin-firebase)](https://coveralls.io/github/dword-design/vuex-entities-plugin-firebase)
+[![Dependency status](https://img.shields.io/david/dword-design/vuex-entities-plugin-firebase)](https://david-dm.org/dword-design/vuex-entities-plugin-firebase)
+![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen)
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/dword-design/vuex-entities-plugin-firebase)
+<!-- /BADGES -->
+
+<!-- DESCRIPTION/ -->
+
+<!-- /DESCRIPTION -->
+
+<!-- INSTALL/ -->
+## Install
+
+```bash
+# NPM
+$ npm install @dword-design/vuex-entities-plugin-firebase
+
+# Yarn
+$ yarn add @dword-design/vuex-entities-plugin-firebase
+```
+<!-- /INSTALL -->
+
+<!-- LICENSE/ -->
+## License
+
+Unless stated otherwise all works are:
+
+Copyright &copy; Sebastian Landwehr <info@dword-design.de>
+
+and licensed under:
+
+[MIT License](https://opensource.org/licenses/MIT)
+<!-- /LICENSE -->
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { forIn, map, mapValues, omit, values } from '@dword-design/functions'
+import firebase from 'firebase/app'
+import { lowerCaseFirst } from 'lower-case-first'
+import objectPath from 'object-path'
+
+export default () => context => {
+  const firestore = firebase.firestore()
+  const auth = firebase.auth()
+  let unsubscribers = []
+  let user
+  const execute = async _user => {
+    user = _user
+    if (user) {
+      unsubscribers =
+        context.types
+        |> mapValues((type, typeName) => {
+          const datePaths = type.datePaths || []
+          return firestore
+            .collection('users')
+            .doc(user.uid)
+            .collection(`${typeName |> lowerCaseFirst}s`)
+            .onSnapshot(snapshot => {
+              const changes =
+                snapshot.docChanges()
+                |> map(docChange => ({
+                  id: docChange.doc.id,
+                  typeName,
+                  ...docChange.doc.data(),
+                  ...(docChange.type === 'removed' && { _deleted: true }),
+                }))
+              changes.forEach(change =>
+                datePaths.forEach(datePath => {
+                  const timestamp = objectPath.get(change, datePath)
+                  if (timestamp !== undefined) {
+                    objectPath.set(change, datePath, timestamp.toDate())
+                  }
+                })
+              )
+              context.store.dispatch('entities/inject', changes)
+            })
+        })
+        |> values
+    } else {
+      forIn(unsubscriber => unsubscriber())(unsubscribers)
+      await context.store.dispatch('entities/reset')
+    }
+  }
+  auth.onAuthStateChanged(execute)
+  return {
+    onPersist: payload => {
+      const batch = firestore.batch()
+      forIn(change => {
+        const ref = firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection(`${change.typeName |> lowerCaseFirst}s`)
+          .doc(change.id)
+        if (change._deleted) {
+          batch.delete(ref)
+        } else {
+          batch.set(
+            ref,
+            change
+              |> omit(['id', 'typeName'])
+              |> mapValues(value =>
+                value === undefined
+                  ? firebase.firestore.FieldValue.delete()
+                  : value
+              ),
+            { merge: true }
+          )
+        }
+      })(payload.changes)
+      return batch.commit()
+    },
+  }
+}
